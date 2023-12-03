aec540ede9d6db67c25d512963f7eb180875f941
sj <sj-git@gruv.org>
2023-12-02 20:36:39 -0500

    spotless

--- build-it.sh
+++ build-it.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
  export PREFIX=~/nethack.mine
+   make spotless
    make WANT_WIN_TTY=1 WANT_WIN_CURSES=1 WANT_WIN_X11=1 PREFIX=~/nethack.mine all
    make WANT_WIN_TTY=1 WANT_WIN_CURSES=1 WANT_WIN_X11=1 PREFIX=~/nethack.mine install
